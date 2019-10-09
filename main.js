7de7a99660527d466eddc57b1e2c9a0ab98b1ffa
SwG Ghosh <codecrafts.cf@icloud.com>
2019-10-09 12:13:26 +0530

    Scripted auto-commit on change (2019-10-09 12:13:26) by gitwatch.sh

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const http = require('http');
 const url = require('url');
 
 function handler(request, response) {
-    
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World!');
 }
 
 var server = http.createServer();
