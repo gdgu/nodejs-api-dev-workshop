1983519cbdfac2385f75e586da69420a6e8739d0
SwG Ghosh <codecrafts.cf@icloud.com>
2019-10-09 12:37:47 +0530

    Scripted auto-commit on change (2019-10-09 12:37:47) by gitwatch.sh

--- alternate.js
+++ alternate.js
@@ -1,3 +1,20 @@
 const http = require('http');
 const url = require('url');
 
+function api(request, response) {
+    var method = request.method;
+    var uri = url.parse(request.url);
+    console.log(method, uri.pathname, 'was requested');
+    if(method == 'POST' && uri.pathname == '/api/message') {
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify(helloWorld));
+    }
+    else if(method == 'GET' && uri.pathname == '/api/messages') {
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify(helloWorld));
+    }
+    else {
+        response.writeHead(404, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify(fourZeroFour));
+    }
+}
